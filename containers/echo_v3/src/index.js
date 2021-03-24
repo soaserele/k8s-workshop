56eb341731195b4bc3876fc493a5925660c163a1
Stanislav Oaserele <soaserele@gmail.com>
2021-03-24 15:44:10 +0200

    Echo v3 service with CPU intensive workload

---
+++ containers/echo_v3/src/index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const { URL } = require('url');
+const crypto = require('crypto');
+const { performance } = require('perf_hooks');
+
+const algorithm = 'sha512';
+const rounds = 64 * 1024;
+
+const params = ['hostname', 'port', 'pathname', 'search'];
+
+const handler = (req, res) => {
+  const start = performance.now();
+
+  let hash = req.url;
+  for (let i = 0; i < rounds; i++) {
+    hash = crypto.createHash(algorithm).update(hash).digest('hex')
+  }
+  
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  const end = performance.now();
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(params.map(param => `<strong>${param}</strong>: ${url[param]}<br/>`).join(''));
+  res.write(`<strong>Checksum</strong>: ${hash}<br/>`);
+  res.write(`<strong>Page generated in</strong>: ${end - start} milliseconds<br/>`);
+  res.end();
+}
+
+http
+  .createServer(handler)
+  .listen(8080, () => console.log('Echo server started'));
